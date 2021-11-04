d74969de96e6e0991cd35db2f2cd5c9c75759b31
HectorLara18 <hectorlaras18@gmail.com>
2021-11-04 17:19:02 -0600

    se agrego informacion de type

--- main.js
+++ main.js
@@ -33,17 +33,29 @@ function requestPokeAPI(searchPokemon) {
 }
 
 function renderData(data) {
+    const types = data.types;
     document.getElementById("pokemonName").innerHTML = `<b>Name: </b> ${data.name}`
+    document.getElementById("pokeType").innerHTML = `<b>type</b> ${renderPokemonType(data)}`
     document.getElementById("hp").innerHTML = `<b>HP: </b> ${data.stats[0].base_stat}`
     document.getElementById("attack").innerHTML = `<b>Attack: </b> ${data.stats[1].base_stat}`
     document.getElementById("deffense").innerHTML = `<b>Deffense: </b> ${data.stats[2].base_stat}`
     document.getElementById("imgPokemon").src = data.sprites.front_default;
     
-    console.log(data)
+    console.log(renderPokemonType(data))
+}
+
+function renderPokemonType(data){
+    const pokemonTypes = [];
+    const types = data.types;
+    types.forEach(element => {
+        pokemonTypes.push(element.type.name)
+    });
+    return pokemonTypes
 }
 
 function renderDataFail(err) {
     document.getElementById("pokemonName").innerHTML = `Pokemon Not Found`
+    document.getElementById("pokeType").innerHTML = `<b>type</b> None`
     document.getElementById("hp").innerHTML = `<b>HP: </b> 9999`
     document.getElementById("attack").innerHTML = `<b>Attack: </b> 9999`
     document.getElementById("deffense").innerHTML = `<b>Deffense: </b> 9999`
